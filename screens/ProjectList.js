9d09202f1bab61753c86f7dabd20d68cf9c97e1b
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-05-09 14:08:50 +0300

    styling create button

--- screens/ProjectList.js
+++ screens/ProjectList.js
@@ -5,15 +5,34 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { color } from "../utilities/Colors";
 
 const { width } = Dimensions.get("window");
 const ProjectList = () => {
   return (
     <View style={styles.container}>
-      <ScrollView>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.cardView}>
+          <TouchableOpacity>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
+              <Text style={styles.titleText}>Title</Text>
+            </View>
+            <View style={styles.descriptionStyle}>
+              <Text>This is a Description about project one</Text>
+            </View>
+            <View>
+              <Text>Wed, May 22, 2024, 12:00 PM</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
             <View
@@ -42,7 +61,12 @@ const ProjectList = () => {
         </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
-            <View style={styles.titleStyle}>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
               <Text
                 style={{
                   color: color.white,
@@ -63,7 +87,12 @@ const ProjectList = () => {
         </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
-            <View style={styles.titleStyle}>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
               <Text
                 style={{
                   color: color.white,
@@ -84,7 +113,12 @@ const ProjectList = () => {
         </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
-            <View style={styles.titleStyle}>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
               <Text
                 style={{
                   color: color.white,
@@ -105,7 +139,12 @@ const ProjectList = () => {
         </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
-            <View style={styles.titleStyle}>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
               <Text
                 style={{
                   color: color.white,
@@ -126,7 +165,38 @@ const ProjectList = () => {
         </View>
         <View style={styles.cardView}>
           <TouchableOpacity>
-            <View style={styles.titleStyle}>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
+              <Text
+                style={{
+                  color: color.white,
+                  fontWeight: "bold",
+                  fontSize: 15,
+                }}
+              >
+                Title
+              </Text>
+            </View>
+            <View style={styles.descriptionStyle}>
+              <Text>This is a Description about project one</Text>
+            </View>
+            <View>
+              <Text>Wed, May 22, 2024, 12:00 PM</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.cardView}>
+          <TouchableOpacity>
+            <View
+              style={[
+                styles.titleStyle,
+                { backgroundColor: true ? color.secondary : color.primary },
+              ]}
+            >
               <Text
                 style={{
                   color: color.white,
@@ -146,6 +216,9 @@ const ProjectList = () => {
           </TouchableOpacity>
         </View>
       </ScrollView>
+      <TouchableOpacity style={styles.createButton}>
+        <Icon name="plus" size={20} color={color.white} />
+      </TouchableOpacity>
     </View>
   );
 };
@@ -156,6 +229,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: color.grayLight,
   },
+  scrollView: {
+    flex: 1,
+  },
   cardView: {
     backgroundColor: color.white,
     width: width * 0.95,
@@ -164,16 +240,31 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   titleStyle: {
-    // backgroundColor: color.secondary,
     padding: 10,
     borderRadius: 5,
     marginBottom: 10,
+  },
+  titleText: {
+    color: color.white,
+    fontWeight: "bold",
+    fontSize: 15,
   },
   descriptionStyle: {
     marginVertical: 5,
     paddingBottom: 15,
     borderBottomColor: color.grayDark,
     borderBottomWidth: 1,
+  },
+  createButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#007bff",
+    borderRadius: 50,
+    padding: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 1,
   },
 });
 
