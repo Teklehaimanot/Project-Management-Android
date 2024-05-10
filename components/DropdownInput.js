4864cea704fa171f67f7615a9e7bac4fe332e266
teklehaimanot <teklehaimanotbelete@gmail.com>
2024-05-10 08:14:15 +0300

    a component for dropdown input is created

---
+++ components/DropdownInput.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import { color } from "../utilities/Colors";
+
+const { width } = Dimensions.get("window");
+const DropdownInput = ({ options, onSelect }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+    setModalVisible(false);
+    onSelect(option);
+  };
+
+  return (
+    <View>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.textInput}
+      >
+        <Text style={styles.labelStyle}>
+          {selectedOption ? selectedOption.label : "Select an option"}
+        </Text>
+      </TouchableOpacity>
+      <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.7)",
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 20,
+              borderRadius: 10,
+              width: width * 0.9,
+            }}
+          >
+            <FlatList
+              data={options}
+              renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => handleSelect(item)}>
+                  <Text
+                    style={{
+                      marginVertical: 8,
+                      fontSize: 15,
+                      fontWeight: "bold",
+                      color: color.statusbar,
+                    }}
+                  >
+                    {item.label}
+                  </Text>
+                </TouchableOpacity>
+              )}
+              keyExtractor={(item) => item.value.toString()}
+            />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginBottom: 10,
+  },
+  labelStyle: {
+    fontWeight: "bold",
+    padding: 6,
+    color: color.statusbar,
+  },
+  textInput: {
+    marginHorizontal: 5,
+    marginVertical: 7,
+    borderRadius: 2,
+    backgroundColor: color.white,
+    padding: 8,
+  },
+});
+
+export default DropdownInput;
--- components/TaskForm.js
+++ components/TaskForm.js
@@ -2,12 +2,24 @@ import React, { useState } from "react";
 import { Dimensions, StyleSheet, View, Text, TextInput } from "react-native";
 import { color } from "../utilities/Colors";
 import Draggable from "react-native-draggable";
+import DropdownInput from "./DropdownInput";
 
 const { width } = Dimensions.get("window");
 const TaskForm = ({ task }) => {
+  const options = [
+    { label: "Option 1", value: 1 },
+    { label: "Option 2", value: 2 },
+    { label: "Option 3", value: 3 },
+  ];
   const [isChecked, setIsChecked] = useState(task.isActive);
   const [title, setTitle] = useState(task.title);
   const [description, setDescription] = useState(task.description);
+
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelectedOption(option);
+  };
   return (
     <View style={styles.cardView}>
       <View style={styles.inputContainer}>
@@ -18,6 +30,9 @@ const TaskForm = ({ task }) => {
           onChangeText={(text) => setTitle(text)}
           value={title}
         />
+      </View>
+      <View style={styles.inputContainer}>
+        <DropdownInput options={options} onSelect={handleSelect} />
       </View>
       <View style={styles.inputContainer}>
         <Text style={styles.labelStyle}>Description</Text>
